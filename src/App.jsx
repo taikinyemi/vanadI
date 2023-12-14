f1c8d134f90a339f03c60361b6d7256dd2a4f66e
taikinyemi <taitolmicky@gmail.com>
2023-12-14 03:20:38 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Courses from "./components/Courses";
+import Reviews from "./components/Reviews";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <main>
+        <div id="home">
+          <Home />
+        </div>
+
+        <div id="about">
+          <About />
+        </div>
+
+        <div id="courses">
+          <Courses />
+        </div>
+
+        <div id="reviews">
+          <Reviews />
+        </div>
+
+        <div id="contact">
+          <Contact />
+        </div>
+        <div id="footer">
+          <Footer />
+        </div>
+
+
+      </main>
+    </div>
+  );
+};
+
+export default App;
